58ca983276f5fe595ee6cc9d225a2520d7e22edc
Nguyễn Phú Quý <83687352+quynguyenepi@users.noreply.github.com>
2024-07-02 14:54:13 +0700

    list of product

--- fontend/src/App.tsx
+++ fontend/src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <head>TS AMAZONE</head>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   )
